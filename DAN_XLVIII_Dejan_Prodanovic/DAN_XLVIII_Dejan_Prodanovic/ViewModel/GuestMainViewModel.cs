9b8d41d9219a0766c1769686702c5baee9437d63
dexpro988 <dejan.prodanovic@panonit.com>
2020-08-03 02:41:02 -0700

    Login page added

---
+++ DAN_XLVIII_Dejan_Prodanovic/DAN_XLVIII_Dejan_Prodanovic/ViewModel/GuestMainViewModel.cs
@@ -0,0 +1,120 @@
+﻿using DAN_XLVIII_Dejan_Prodanovic.Command;
+using DAN_XLVIII_Dejan_Prodanovic.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace DAN_XLVIII_Dejan_Prodanovic.ViewModel
+{
+    class GuestMainViewModel:ViewModelBase
+    {
+        GuestMainView guestMainView;
+        private string JMBG;
+        #region Constructors
+        public GuestMainViewModel(GuestMainView guestMainViewOpen, string JMBG)
+        {
+            guestMainView = guestMainViewOpen;
+            this.JMBG = JMBG;
+        }
+        #endregion
+
+        #region Commands
+        private ICommand showMenu;
+        public ICommand ShowMenu
+        {
+            get
+            {
+                if (showMenu == null)
+                {
+                    showMenu = new RelayCommand(param => ShowMenuExecute(), param => CanShowMenuExecute());
+                }
+                return showMenu;
+            }
+        }
+
+        private void ShowMenuExecute()
+        {
+            try
+            {
+                //MenuView menuView = new MenuView(JMBG);
+                //menuView.Show();
+                //guestMainView.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+        private bool CanShowMenuExecute()
+        {
+            return true;
+        }
+
+        private ICommand logoutCommmand;
+        public ICommand LogoutCommmand
+        {
+            get
+            {
+                if (logoutCommmand == null)
+                {
+                    logoutCommmand = new RelayCommand(param => LogoutExecute(), param => CanLogoutExecute());
+                }
+                return logoutCommmand;
+            }
+        }
+
+        private void LogoutExecute()
+        {
+            try
+            {
+                LoginView loginView = new LoginView();
+                guestMainView.Close();
+                loginView.ShowDialog();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+        private bool CanLogoutExecute()
+        {
+            return true;
+        }
+
+        private ICommand close;
+        public ICommand Close
+        {
+            get
+            {
+                if (close == null)
+                {
+                    close = new RelayCommand(param => CloseExecute(), param => CanCloseExecute());
+                }
+                return close;
+            }
+        }
+
+        private void CloseExecute()
+        {
+            try
+            {
+
+                guestMainView.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+        private bool CanCloseExecute()
+        {
+            return true;
+        }
+        #endregion
+    }
+}
